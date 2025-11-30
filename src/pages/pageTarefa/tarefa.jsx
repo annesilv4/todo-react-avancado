516ca05adc88af8e341ed1b3078d982244bd1b0c
annesilv4 <carolaynebarbozasilva@gmail.com>
2025-11-30 18:11:24 -0300

    Adiciona página de tarefas com estilos e estrutura inicial

---
+++ src/pages/pageTarefa/tarefa.jsx
@@ -0,0 +1,26 @@
+import "./tarefa.css";
+import { Link } from "react-router-dom";
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+import FiltrarTarefa from "../../components/filterTarefa/filterTarefa";
+
+export default function Tarefa() {
+    return (
+        <>
+            <Header />
+
+            <main className="user__tarefas">
+                <h1 className="user__title">Suas Tarefas</h1>
+                <div className="user__action">
+                    <FiltrarTarefa />
+                </div>
+
+                <div className="user__tasks">
+                    <p className="user__noTasks">Nenhuma tarefa cadastrada</p>
+                </div>
+            </main>
+
+            <Footer />
+        </>
+    )
+}
