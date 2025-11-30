ee10b4e0cf94f3ad4bafb507efdce30d9664ff8f
annesilv4 <carolaynebarbozasilva@gmail.com>
2025-11-30 18:11:37 -0300

    Adiciona componente de rodapé com estilos

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import "./footer.css";
+
+export default function Footer() {
+    return (
+        <>
+            <footer className="footer__todo">
+                <h1 className="footer__title">Todo <span>List</span></h1>
+                <div className="separetor"></div>
+                <p className="footer__copy">&copy; 2025 - Todos os direitos reservados.</p>
+            </footer>
+        </>
+    )
+}
