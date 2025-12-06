c98129936741ae69014def9d335a152a95f50e98
annesilv4 <carolaynebarbozasilva@gmail.com>
2025-12-06 19:06:50 -0300

    Adicionando verificação para saber se o usuário esrá logado ou não

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,6 +3,9 @@ import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Header() {
+    const isLogged = localStorage.getItem("token"); // <-- verificação
+    const userName = localStorage.getItem("userName"); // opcional
+
     return (
         <header className="header__page">
             <div className="header__logo">
@@ -10,12 +13,26 @@ export default function Header() {
                 <p className="header__subtitle">Seu gerenciador de tarefas do dia a dia</p>
             </div>
 
-            <div className="header__user">
-                <div className="header__avatar">
-                    <FontAwesomeIcon icon={faUser} className="header__icon" />
+            {isLogged ? (
+                // SE ESTÁ LOGADO
+                <div className="header__user">
+                    <div className="header__avatar">
+                        <FontAwesomeIcon icon={faUser} className="header__icon" />
+                    </div>
+                    <p className="header__name">
+                        Olá, {userName ? userName : "Usuário"}
+                    </p>
                 </div>
-                <p className="header__name">Olá Usuário</p>
-            </div>
+
+            ) : (
+                // SE NÃO ESTÁ LOGADO
+                <button
+                    className="header__loginButton"
+                    onClick={() => (window.location.href = "/login")}
+                >
+                    Entrar
+                </button>
+            )}
         </header>
     )
 }
