50681c7d5a7ff9780f6829aefe4a9a2eeed40c30
annesilv4 <carolaynebarbozasilva@gmail.com>
2025-12-07 13:07:45 -0300

    Fazendo a navegação do login

--- src/pages/pageLogin/pageLogin.jsx
+++ src/pages/pageLogin/pageLogin.jsx
@@ -1,17 +1,44 @@
 import { useState } from "react";
-
-import FormLoginUser from "../../components/formUser/formUser.jsx";
-import FormNewUser from "../../components/formNewUser/formNewUser.jsx";
+import { useNavigate } from "react-router-dom";
+import { createUser, loginUser } from "../../api/apiUsuario";
+import FormLoginUser from "../../components/formUser/formUser";
+import FormNewUser from "../../components/formNewUser/formNewUser";
 
 export default function PageLogin() {
     const [isNew, setIsNew] = useState(false);
+    const navigate = useNavigate();
+
+    const handleLogin = async (email, password) => {
+        const user = await loginUser(email, password);
+
+        if (!user) {
+            console.error("Email ou senha inválidos");
+            return;
+        }
+
+        // ⭐ SALVA LOGIN AQUI ⭐
+        localStorage.setItem("token", user._id);
+        localStorage.setItem("userName", user.name);
+
+        console.log("Login realizado com sucesso");
+
+        // Redireciona para Home / Tarefas
+        navigate("/");
+    };
+
+    const handleRegister = async (formData) => {
+        await createUser(formData);
+        console.log("Usuário criado com sucesso");
+
+        setIsNew(false);
+    };
 
     return (
         <>
             {isNew ? (
-                <FormNewUser onBackToLogin={() => setIsNew(false)} />
+                <FormNewUser onBackToLogin={() => setIsNew(false)} onRegister={handleRegister} />
             ) : (
-                <FormLoginUser onGoToRegister={() => setIsNew(true)} />
+                <FormLoginUser onGoToRegister={() => setIsNew(true)} onLogin={handleLogin} />
             )}
         </>
     );
