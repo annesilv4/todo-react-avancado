47a015b31e5b10d25bbbfba7614c7a0c14338459
annesilv4 <carolaynebarbozasilva@gmail.com>
2025-12-13 11:40:34 -0300

    buscando as tarefas e fazendo as formatações necessarias para deixa-las organizadas

--- src/pages/pageTarefa/tarefa.jsx
+++ src/pages/pageTarefa/tarefa.jsx
@@ -1,10 +1,28 @@
 import "./tarefa.css";
-import { Link } from "react-router-dom";
 import Header from "../../components/header/header";
 import Footer from "../../components/footer/footer";
 import FiltrarTarefa from "../../components/filterTarefa/filterTarefa";
+import { useState, useEffect } from "react";
+import { getTask } from "../../api/apiTarefa";
 
 export default function Tarefa() {
+    const [tasks, setTasks] = useState([]);
+    // const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchTarefas = async () => {
+            try {
+                const request = await getTask();
+                setTasks(request);
+            } catch (err) {
+                console.error("[ ERROR TASKS ]", err);
+            } finally {
+                // setLoading(false);
+            }
+        };
+        fetchTarefas();
+    }, [])
+
     return (
         <>
             <title>Todo List - Tarefas</title>
@@ -18,7 +36,22 @@ export default function Tarefa() {
                 </div>
 
                 <div className="user__tasks">
-                    <p className="user__noTasks">Nenhuma tarefa cadastrada</p>
+                    {tasks.length == 0 ? (
+                        <p className="user__noTasks">Nenhuma tarefa encontrada</p>
+                    ) : (
+                        tasks.map(task => (
+                            <div key={task._id} className="user__task">
+                                <div className="user__taskCheckbox">
+                                    <input type="checkbox" />
+                                </div>
+                                <div>
+                                    <h2>{task.title}</h2>
+                                    <p>{task.description}</p>
+                                    <small>{task.dateStart}</small>
+                                </div>
+                            </div>
+                        ))
+                    )}
                 </div>
             </main>
 
