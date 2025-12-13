d7c4e3d941008bef3169a86c017060319c2dc942
annesilv4 <carolaynebarbozasilva@gmail.com>
2025-12-13 10:53:21 -0300

    Adicionando formatação das informações das tarefas

--- src/components/formTarefa/formTarefa.jsx
+++ src/components/formTarefa/formTarefa.jsx
@@ -2,13 +2,31 @@ import "./formTarefa.css"
 import Header from "../header/header";
 import Footer from "../footer/footer";
 import useInput from "../../hooks/useInput";
+import { useNavigate } from "react-router-dom";
 
-export default function FormTarefa() {
+export default function FormTarefa({ onSubmit }) {
     const task = useInput("");
     const description = useInput("");
     const dateStart = useInput("");
     const dateEnd = useInput("");
     const time = useInput("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newTask = {
+            title: task.valor,
+            description: description.valor,
+            dateStart: dateStart.valor,
+            dateEnd: dateEnd.valor,
+            time: time.valor,
+            status: "uncompleted",
+        };
+
+        onSubmit(newTask);
+    }
+
+    const navigate = useNavigate();
 
 
     return (
@@ -20,34 +38,34 @@ export default function FormTarefa() {
                 <h1 className="formTarefa__title">Descreva sua Tarefa</h1>
 
                 <div className="form">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className="form__title">
                             <label htmlFor="tarefa">Título da Tarefa</label>
-                            <input type="text" id="tarefa" name="tarefa" value={task.value} onChange={task.onChange} />
+                            <input type="text" id="tarefa" name="tarefa" value={task.valor} onChange={task.onChange} />
                         </div>
 
                         <div className="form__description">
                             <label htmlFor="description__task">Descrição da Tarefa</label>
-                            <textarea id="description__task" name="description__task" value={description.value} onChange={description.onChange}></textarea>
+                            <textarea id="description__task" name="description__task" value={description.valor} onChange={description.onChange}></textarea>
                         </div>
 
                         <div className="form__data">
                             <div className="form__dateStart">
                                 <label htmlFor="data">Data de Início</label>
-                                <input type="date" id="data" name="data" value={dateStart.value} onChange={dateStart.onChange} />
+                                <input type="date" id="data" name="data" value={dateStart.valor} onChange={dateStart.onChange} />
                             </div>
                             <div className="form__dateEnd">
                                 <label htmlFor="data">Data de Fim</label>
-                                <input type="date" id="data" name="data" value={dateEnd.value} onChange={dateEnd.onChange} />
+                                <input type="date" id="data" name="data" value={dateEnd.valor} onChange={dateEnd.onChange} />
                             </div>
                         </div>
 
                         <div className="form__time">
                             <label htmlFor="hora">Horario de Entrega</label>
-                            <input type="time" id="hora" name="hora" value={time.value} onChange={time.onChange} />
+                            <input type="time" id="hora" name="hora" value={time.valor} onChange={time.onChange} />
                         </div>
 
-                        <button className="btn__newTask" type="submit">Criar Tarefa</button>
+                        <button className="btn__newTask" type="submit" onClick={() => navigate("/")}>Criar Tarefa</button>
                     </form>
                 </div>
             </main>
