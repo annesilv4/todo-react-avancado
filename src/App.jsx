e6470f39b29271840732131007391cd79b049a66
annesilv4 <carolaynebarbozasilva@gmail.com>
2025-12-14 07:21:41 -0300

    Adicionandi uma função handler assíncrona para criação de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,20 @@ import Tarefa from './pages/pageTarefa/tarefa';
 import PageLogin from './pages/pageLogin/pageLogin';
 import FormNewUser from './components/formNewUser/formNewUser';
 import FormTarefa from './components/formTarefa/formTarefa';
+import { createTask } from './api/apiTarefa';
 
 
 function App() {
+    // FUNCTION PARA CRIAR UMA NOVA TAREFA
+    const handleCreateTask = async (taskData) => {
+        try {
+            await createTask(taskData);
+            window.location.href = "/";
+        } catch (err) {
+            console.error("Error creating task:", err);
+        }
+    };
+
     return (
         <BrowserRouter>
             <Routes>
@@ -20,7 +31,7 @@ function App() {
                 <Route path='/formNewUser' element={<FormNewUser />} />
 
                 {/* Rota de Fazer Tarefa */}
-                <Route path='/formTarefa' element={<FormTarefa />} />
+                <Route path='/formTarefa' element={<FormTarefa onSubmit={handleCreateTask} />} />
 
             </Routes>
         </BrowserRouter>
